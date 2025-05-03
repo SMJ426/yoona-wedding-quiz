0f31db929040922d16e6920094af5eb8f1f4d2c7
SMJ426 <jerry04260@gmail.com>
2025-05-03 22:38:03 +0900

    feat: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <link rel="icon" href="/images/favicon.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
