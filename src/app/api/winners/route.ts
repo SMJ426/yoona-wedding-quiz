0642736e3d271567f09ebf0aed8080b6ec4a2180
SMJ426 <jerry04260@gmail.com>
2025-05-08 00:22:19 +0900

    feat: 추첨 페이지 개발

---
+++ src/app/api/winners/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import clientPromise from '../../../../lib/mongodb';
+
+export async function GET() {
+  const client = await clientPromise;
+  const db = client.db('wedding_quiz');
+  const col = db.collection('entries');
+
+  // 전체 목록 가져오기
+  const allEntries = await col
+    .find({}, { projection: { name: 1, phone: 1, score: 1, _id: 0 } })
+    .toArray();
+
+  // 1) 전체 인원 중 랜덤 추출
+  const randomFromAll =
+    allEntries[Math.floor(Math.random() * allEntries.length)];
+
+  // 2) 3개 다 맞춘 인원 중 랜덤 추출
+  const perfectEntries = allEntries.filter((entry) => entry.score === 3);
+  const randomFromPerfect =
+    perfectEntries.length > 0
+      ? perfectEntries[Math.floor(Math.random() * perfectEntries.length)]
+      : null;
+
+  return NextResponse.json({
+    perfectScoreWinner: randomFromPerfect,
+    randomWinner: randomFromAll,
+  });
+}
---
+++ src/app/draw/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+interface WinnerItem {
+  name: string;
+  phone: string;
+  score: number;
+}
+
+interface Winner {
+  perfectScoreWinner: WinnerItem | null;
+  randomWinner: WinnerItem;
+}
+const ScreenDraw: React.FC = ({}) => {
+  const [winners, setWinners] = useState<Winner | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('/api/winners', {
+      method: 'GET',
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data: Winner) => {
+        setWinners(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError('당첨자 정보를 불러오지 못했습니다.');
+      });
+  }, []);
+
+  if (error) return <p className="text-red-500">{error}</p>;
+
+  return (
+    <div className="flex flex-col">
+      <p>3개 다 맞춘사람: {winners?.perfectScoreWinner?.name}</p>
+      <p>전화번호: {winners?.perfectScoreWinner?.phone}</p>
+      <p>전체 당첨자 중 한명: {winners?.randomWinner?.name}</p>
+      <p>전화번호: {winners?.randomWinner?.phone}</p>
+    </div>
+  );
+};
+
+export default ScreenDraw;
