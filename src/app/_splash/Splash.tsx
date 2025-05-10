28989cbc4ab07f61b12b62c1c237f5b06c885cb4
SMJ426 <jerry04260@gmail.com>
2025-05-10 22:57:04 +0900

    feat: Splash image change

--- src/app/_splash/Splash.tsx
+++ src/app/_splash/Splash.tsx
@@ -6,27 +6,19 @@ import { useEffect } from 'react';
 const ScreenSplashContent: React.FC = () => {
   const router = useRouter();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/quiz/explanation');
-    }, 3500);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.push('/quiz/explanation');
+  //   }, 3500);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
     <div
       className="flex items-center justify-center w-screen h-screen bg-center bg-no-repeat bg-cover animate-fade-in-splash"
       style={{ backgroundImage: "url('/images/Splash_image.webp')" }}
-    >
-      <div className="mt-[350px] text-center text-white">
-        <h1 className="text-[40px] mb-[10px]">유나와 희도</h1>
-        <div className="flex flex-col">
-          <span className="px-4 text-[30px]">"퀴즈와 함께하는</span>
-          <span className="px-4 text-[30px]">특별한 순간"</span>
-        </div>
-      </div>
-    </div>
+    ></div>
   );
 };
 
