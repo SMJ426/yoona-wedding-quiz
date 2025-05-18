56393d09a562d488447a8771efd52e4c1c449f87
SMJ426 <jerry04260@gmail.com>
2025-05-18 21:04:22 +0900

    style: 글 중앙 정렬

--- src/app/blockeduser/page.tsx
+++ src/app/blockeduser/page.tsx
@@ -6,7 +6,7 @@ const ScreenBlockedUser: React.FC = ({}) => {
       <h3 className="text-2xl text-center">
         퀴즈는 한번만 참여할 수 있어요 🥲
       </h3>
-      <div className="flex flex-col items-center justify-center text-lg">
+      <div className="flex flex-col items-center justify-center text-lg text-center">
         <p>퀴즈 참여에 어려움이 있거나</p>
         <p>오류가 있다면</p>
         <p className="pt-5 text-xl">📞 010-2369-3763으로 연락주세요.</p>
